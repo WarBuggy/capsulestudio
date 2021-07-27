6d7b59b88b1765ba7275c46b2ed75b6609e5ee16
hovanbuu <hovanbuu@gmail.com>
2021-07-27 17:37:43 +0700

    _ add minWaitTime for preloading image

--- public/script/index.js
+++ public/script/index.js
@@ -3,7 +3,7 @@ window.onload = async function() {
     let loadingInitial = new LoadingInitial();
     document.body.appendChild(loadingInitial.div);
     let imagePreloadCategoryList = ['home', ];
-    Common.preloadImageFromVersion(imagePreloadCategoryList, 0, function() {
+    Common.preloadImageFromVersion(imagePreloadCategoryList, 3000, function() {
         document.body.removeChild(loadingInitial.div);
         createDivHomeBanner();
         new ContentHome();
